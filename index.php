26479b04ec7beb9e17f30f8d2c92c895b9b54174
xuguilin <1937767846@qq.com>
2016-06-14 17:24:07 +0800

    xindongxi

--- index.php
+++ index.php
@@ -45,27 +45,37 @@
 // $url 是远程图片的完整URL地址，不能为空。
 // $filename 是可选变量: 如果为空，本地文件名将基于时间和日期
 // 自动生成.
-function GrabImage($url,$filename="") {
-if($url==""):return false;endif;
-if($filename=="") {
-$ext=strrchr($url,".");
-if($ext!=".gif" && $ext!=".png"):return false;endif;
-$filename=date("Ymd").$ext;
-}
-ob_start();
-readfile($url);
-$img = ob_get_contents();
-ob_end_clean();
-$size = strlen($img);
-$fp2=@fopen($filename, "a");
-fwrite($fp2,$img);
-fclose($fp2);
-return $filename;
-}
-$img=GrabImage("http://www.baidu.com/img/bd_logo1.png","");
-if($img):echo '<pre><img src="'.$img.'"></pre>'; 
-else:echo "false";
-endif;
+//function GrabImage($url,$filename="") {
+//if($url==""):return false;endif;
+//if($filename=="") {
+//$ext=strrchr($url,".");
+//if($ext!=".gif" && $ext!=".png"):return false;endif;
+//$filename=date("Ymd").$ext;
+//}
+//ob_start();
+//readfile($url);
+//$img = ob_get_contents();
+//ob_end_clean();
+//$size = strlen($img);
+//$fp2=@fopen($filename, "a");
+//fwrite($fp2,$img);
+//fclose($fp2);
+//return $filename;
+//}
+//$img=GrabImage("http://www.baidu.com/img/bd_logo1.png","");
+//if($img):echo '<pre><img src="'.$img.'"></pre>'; 
+//else:echo "false";
+//endif;
 print_r("好开心啊！终于对git搞懂一点了");
-
+echo "<br/>";
+//$city = "上海";
+//$name = "林子";
+//$nothing_here = "是啊，什么都没有";
+//$location_vars = array('city','name');
+//$result = compact('event','nothing_here',$location_vars);
+//print_r($result);
+$input = array("red", "green", "blue", "yellow");
+array_splice($input,-2,2);
+print_r($input);echo "<br/>";
+print_r(array_search('red', $input));
 ?>
