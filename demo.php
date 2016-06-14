2467c198d0d53c5dc9af3dac7e14d701eba53872
xuguilin <1937767846@qq.com>
2016-06-14 11:49:05 +0800

    开心

---
+++ demo.php
@@ -0,0 +1,18 @@
+<?php
+
+function myTest()
+{
+static $x=0;
+echo $x;
+$x++;
+}
+
+myTest();
+echo "<br>";
+myTest();
+echo "<br>";
+myTest();
+echo "<br>";
+myTest();
+echo "<br>";
+myTest();
---
+++ index.php
@@ -0,0 +1,70 @@
+<!--<!DOCTYPE html>
+<html>
+    <head>
+        <script     src="/jquery-1.7.2.min.js"></script>
+    </head>
+    
+    <body>
+
+        <div class="content">
+            
+            </div>
+        <input type="button" name="点击" value="lal" id="bt"/>
+        <Script>
+         
+               alert('dfsf');
+              $("div").data("blah",'hello'); 
+              $('#bt').click(function(){
+                  alert($("div").data("blah"));
+              });
+        </script> 
+    </body>
+        
+</html>
+-->
+<?php
+//// readfile('http://www.baidu.com/img/bd_logo1.png', 'r');
+//$file = "http://www.baidu.com/img/bd_logo1.png";
+//print_r(strrchr($file, "."));
+//
+////$email  = 'name@example.com';
+////$domain = strstr($email, '@');
+////echo $domain; // 打印 @example.com
+////
+////$user = strstr($email, '@', true); // 从 PHP 5.3.0 起
+////echo " ".$user; // 打印 name
+//
+//
+// Function: 获取远程图片并把它保存到本地
+//
+//
+// 确定您有把文件写入本地服务器的权限
+//
+//
+// 变量说明:
+// $url 是远程图片的完整URL地址，不能为空。
+// $filename 是可选变量: 如果为空，本地文件名将基于时间和日期
+// 自动生成.
+function GrabImage($url,$filename="") {
+if($url==""):return false;endif;
+if($filename=="") {
+$ext=strrchr($url,".");
+if($ext!=".gif" && $ext!=".png"):return false;endif;
+$filename=date("Ymd").$ext;
+}
+ob_start();
+readfile($url);
+$img = ob_get_contents();
+ob_end_clean();
+$size = strlen($img);
+$fp2=@fopen($filename, "a");
+fwrite($fp2,$img);
+fclose($fp2);
+return $filename;
+}
+$img=GrabImage("http://www.baidu.com/img/bd_logo1.png","");
+if($img):echo '<pre><img src="'.$img.'"></pre>'; 
+else:echo "false";
+endif;
+
+?>
